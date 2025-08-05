6849f3a15a2537ffaeaf4c89a60a79de566cd51f
sajib9988 <sajib.biswas9988@gmail.com>
2025-08-05 11:53:11 +0600

    u77

--- Server/src/app/modules/paypal/utils/PaypalAccessToken.ts
+++ Server/src/app/modules/paypal/utils/PaypalAccessToken.ts
@@ -1,21 +1,19 @@
+export const getAccessToken = async () => {
+  const response = await fetch(`${process.env.paypal_Base_URL}/oauth2/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${Buffer.from(
+        `${process.env.client_Id}:${process.env.client_Secret}`
+      ).toString("base64")}`,
+    },
+    body: "grant_type=client_credentials",
+  });
 
+  if (!response.ok) {
+    throw new Error("Failed to fetch PayPal access token");
+  }
 
-
- export const  getAccessToken= async () => {
-        const response = await fetch(`${process.env.paypal_Base_URL}/oauth2/token`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: `Basic ${Buffer.from(`${process.env.client_Id}:${process.env.client_Secret}`).toString('base64')}`,
-        },
-        body: 'grant_type=client_credentials',
-        });
-    
-        if (!response.ok) {
-        throw new Error('Failed to fetch PayPal access token');
-        }
-    
-        const data = await response.json();
-        return data.access_token;
-    }
-
+  const data = await response.json();
+  return data.access_token;
+};
